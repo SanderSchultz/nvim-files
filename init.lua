775e961df6ec5fa60a678f6d4f03f95242991d61
SanderSchultz <schultz.sander@gmail.com>
2024-07-15 10:58:36 +0200

    Added toggling terminal ctrl + b

--- init.lua
+++ init.lua
@@ -143,6 +143,34 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup {
 
     -- Use `opts = {}` to force a plugin to be loaded.
+      {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        config = function()
+          require("toggleterm").setup{
+            size = 10,
+            open_mapping = [[<c-b>]],
+            hide_numbers = true,
+            shade_filetypes = {},
+            shade_terminals = true,
+            shading_factor = '1',
+            start_in_insert = true,
+            insert_mappings = true,
+            persist_size = true,
+            direction = 'horizontal',
+            close_on_exit = true,
+            shell = vim.o.shell,
+            float_opts = {
+              border = 'curved',
+              winblend = 0,
+              highlights = {
+                border = "Normal",
+                background = "Normal",
+              }
+            }
+          }
+        end
+      },
     {'kdheepak/lazygit.nvim',
         cmd = {
             "LazyGit",
