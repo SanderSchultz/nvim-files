534af3459e8bfaf488be5d759a7f8efe9c701f19
Schultz <schultz.sander@gmail.com>
2025-05-24 00:24:47 +0200

    added barbar, might remove

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,15 @@
 ---@diagnostic disable: undefined-global
 
-vim.keymap.set("c", "?", function()
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+local silent = { silent = true }
+local no_remap = { noremap = true }
+
+map('n', '<M-h>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<M-l>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<M-w>', '<Cmd>BufferClose<CR>', opts)
+
+map("c", "?", function()
   if vim.fn.getcmdpos() == 1 then
     return vim.api.nvim_replace_termcodes("Oil oil-ssh://", true, false, true)
   end
@@ -8,7 +17,7 @@ vim.keymap.set("c", "?", function()
 end, { expr = true, noremap = true, desc = "Oil-SSH prefix when cmdline is empty" })
 
 -- In command‐line mode, make <CR> “smart” for Oil-SSH URIs
-vim.keymap.set("c", "<CR>", function()
+map("c", "<CR>", function()
   if vim.fn.getcmdline():match("^Oil%s+oil%-ssh://[^/]+$") then
     return vim.api.nvim_replace_termcodes("/\r", true, false, true)
   end
@@ -16,83 +25,82 @@ vim.keymap.set("c", "<CR>", function()
 end, { expr = true, noremap = true, desc = "Oil-SSH: append slash before Enter" })
 
 -- See diagnostics of error, Ctrl + e
-vim.keymap.set('n', '<C-e>', ':lua vim.diagnostic.open_float(nil, {focus = false})<CR>',
-	{ noremap = true, silent = true })
+map('n', '<C-e>', ':lua vim.diagnostic.open_float(nil, {focus = false})<CR>', opts)
 
 --Rename all variables, Ctrl + r
-vim.keymap.set('n', '<C-r>', ':%s/')
+map('n', '<C-r>', ':%s/')
 
 --Undoes the undo (U undoes u)
-vim.keymap.set("n", "U", function()
+map("n", "U", function()
 	vim.cmd("redo")
-end, { noremap = true, silent = true })
+end, opts)
 
 --Changes keymapping for end of line and beginning of line
-vim.keymap.set('n', '<Space>', '$', { noremap = true })
-vim.keymap.set('n', 'n', '^', { noremap = true })
+map('n', '<Space>', '$', no_remap)
+map('n', 'n', '^', no_remap)
 
 --Toggles between last 2 files in buffer
-vim.keymap.set('n', '<C-f>', '<C-^>')
+map('n', '<C-f>', '<C-^>')
 
 --Replaces C-w to ALT + these keys, for windows movement and splitting
-local map = vim.keymap.set
-map('n', '<M-h>', '<C-w>h')
-map('n', '<M-j>', '<C-w>j')
-map('n', '<M-k>', '<C-w>k')
-map('n', '<M-l>', '<C-w>l')
-map('n', '<M-w>', '<C-w>w')
-map('n', '<M-s>', '<C-w>s') -- Horizontal split
-map('n', '<M-v>', '<C-w>v') -- Vertical split
+--THESE CONFLICT WITH BARBAR SHORTCUTS
+-- map('n', '<M-h>', '<C-w>h')
+-- map('n', '<M-j>', '<C-w>j')
+-- map('n', '<M-k>', '<C-w>k')
+-- map('n', '<M-l>', '<C-w>l')
+-- map('n', '<M-w>', '<C-w>w')
+-- map('n', '<M-s>', '<C-w>s') -- Horizontal split
+-- map('n', '<M-v>', '<C-w>v') -- Vertical split
 
 --Changes / search to 's'
-vim.keymap.set('n', 's', '/', { noremap = true })
+map('n', 's', '/', no_remap)
 
 --Formats file by LSP standard
-vim.keymap.set('n', '=G', ':lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
+map('n', '=G', ':lua vim.lsp.buf.format()<CR>', opts)
 
 --Changes Ctrl+n to move to next instance in search, SHIFT+n to go backwards
-vim.keymap.set('n', '<C-n>', 'n', { noremap = true });
+map('n', '<C-n>', 'n', no_remap);
 
 --To unmark what has been marked by search
-vim.keymap.set('n', '<Esc>', ':nohlsearch<CR><Esc>', { noremap = true, silent = true })
+map('n', '<Esc>', ':nohlsearch<CR><Esc>', opts)
 
 -- Opens Oil
-vim.keymap.set('n', '-', '<cmd>Oil<CR>')
+map('n', '-', '<cmd>Oil<CR>')
 
 --Moves highlighted lines up and down inside the file
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+map('v', 'J', ":m '>+1<CR>gv=gv")
+map('v', 'K', ":m '<-2<CR>gv=gv")
 
 --Makes what is highlighted copied or pasted from/to clipboard
-vim.keymap.set('n', 'y', '"+y')
-vim.keymap.set('v', 'y', '"+y')
-vim.keymap.set('n', 'Y', '"+Y')
+map('n', 'y', '"+y')
+map('v', 'y', '"+y')
+map('n', 'Y', '"+Y')
 
-vim.keymap.set('n', 'p', '"+p')
-vim.keymap.set('v', 'p', '"+p')
-vim.keymap.set('n', 'P', '"+P')
+map('n', 'p', '"+p')
+map('v', 'p', '"+p')
+map('n', 'P', '"+P')
 
 --Make deleting files interact with clipboard
-vim.keymap.set('n', 'dd', '"+dd', { noremap = true })
-vim.keymap.set('v', 'd', '"+d', { noremap = true })
+map('n', 'dd', '"+dd', no_remap)
+map('v', 'd', '"+d', no_remap)
 
 --Sets Ctrl + z to go back just like 'u'
-vim.keymap.set('n', '<C-z>', ':undo<CR>', { noremap = true })
+map('n', '<C-z>', ':undo<CR>', no_remap)
 
 --Sets Ctrl + s to save file like :w
-vim.keymap.set('n', '<C-s>', '<Cmd>update<CR>', { silent = true })
-vim.keymap.set('i', '<C-s>', '<Esc><Cmd>update<CR>a', { silent = true })
+map('n', '<C-s>', '<Cmd>update<CR>', silent)
+map('i', '<C-s>', '<Esc><Cmd>update<CR>a', silent)
 
 --Sets Ctrl + s to save file like :w
-vim.keymap.set('n', '<C-q>', '<Cmd>quit<CR>', { silent = true })
-vim.keymap.set('i', '<C-q>', '<Esc><Cmd>quit<CR>a', { silent = true })
+map('n', '<C-q>', '<Cmd>quit<CR>', silent)
+map('i', '<C-q>', '<Esc><Cmd>quit<CR>a', silent)
 
 --Sets mappings to complete parentheses and quotes
-vim.keymap.set('i', '(', '()<Left>', {})
-vim.keymap.set('i', '[', '[]<Left>', {})
-vim.keymap.set('i', '{', '{}<Left>', {})
-vim.keymap.set('i', '"', '""<Left>', {})
-vim.keymap.set('i', '\'', '\'\'<Left>', {})
+map('i', '(', '()<Left>', {})
+map('i', '[', '[]<Left>', {})
+map('i', '{', '{}<Left>', {})
+map('i', '"', '""<Left>', {})
+map('i', '\'', '\'\'<Left>', {})
 
 --Opens the Undotree
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+map('n', '<leader>u', vim.cmd.UndotreeToggle)
---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,13 @@
+return {
+	'romgrk/barbar.nvim',
+	dependencies = {
+		'nvim-tree/nvim-web-devicons',
+	},
+	opts = {
+		animation         = false,
+		icons             = {
+			separator = { left = ' ', right = ' ' },
+			separator_at_end = false,
+		},
+	},
+}
